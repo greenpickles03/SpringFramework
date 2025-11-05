6c5750f7a49b4202d16e8a111a1f55e0a98fb4ba
greenpickles <greenpickles03@github.com>
2025-11-05 10:58:59 +0800

    update

--- src/main/java/com/springframework/Spring/Framework/SpringFrameworkApplication.java
+++ src/main/java/com/springframework/Spring/Framework/SpringFrameworkApplication.java
@@ -2,12 +2,18 @@ package com.springframework.Spring.Framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringFrameworkApplication {
+public class SpringFrameworkApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringFrameworkApplication.class, args);
+        SpringApplication.run(SpringFrameworkApplication.class, args);
 	}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringFrameworkApplication.class);
+    }
 }
